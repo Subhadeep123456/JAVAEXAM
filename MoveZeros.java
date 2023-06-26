be0769a23acdeed6cbaaa7b7047357a779aa349d
Subhadeep123456 <86153899+Subhadeep123456@users.noreply.github.com>
2023-06-27 00:22:10 +0530

    Create MoveZeros.java

---
+++ MoveZeros.java
@@ -0,0 +1,16 @@
+public class MoveZero {
+     public void moveZeroes(int[] nums) {
+        int p=nums.length-1;
+        for(int k=0;k<=p;k++){
+            for(int i=0;i<p;i++){
+                if(nums[i]==0){
+                    for(int j=i;j<p;j++){
+                        nums[j]=nums[j+1];
+                    }
+                        nums[p]=0;
+                        p--;
+                }
+            }
+        }
+    }
+}
